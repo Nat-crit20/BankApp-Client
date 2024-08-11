70ac2ab326d6644f4305db7899ae5b9fa360a45c
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-11 16:04:59 -0500

    Created method for exchanging tokens

--- src/Components/Link.tsx
+++ src/Components/Link.tsx
@@ -1,11 +1,30 @@
+import { useCallback } from "react";
 import { usePlaidLink } from "react-plaid-link";
 
 interface LinkProps {
   linkToken: string;
 }
 function Link({ linkToken }: LinkProps): JSX.Element {
+  const onSuccess = useCallback((public_token: string) => {
+    const exchangePublicTokenForAccessToken = async () => {
+      const response = await fetch("/api/set_access_token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+        body: `public_token=${public_token}`,
+      });
+      if (!response.ok) {
+        return;
+      }
+      const data = await response.json();
+      console.log(data);
+    };
+  }, []);
+
   const { open, ready } = usePlaidLink({
     token: linkToken,
+    onSuccess,
   });
   return (
     <button onClick={() => open()} disabled={!ready}>
