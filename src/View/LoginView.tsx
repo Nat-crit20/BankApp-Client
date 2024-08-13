e5c1f00e8de681301773b533ad2d4c19c0a90f26
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-13 14:51:52 -0500

    Create the login and sign up template

---
+++ src/View/LoginView.tsx
@@ -0,0 +1,8 @@
+function LoginView() {
+  return (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+}
+export default LoginView;
---
+++ src/View/SignUpView.tsx
@@ -0,0 +1,9 @@
+function SignUpView() {
+    return (
+      <>
+       
+        <h1>Sign Up</h1>
+      </>
+    );
+  }
+  export default SignUpView;
