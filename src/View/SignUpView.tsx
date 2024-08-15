8ac2ccf32a71af7ad6a6de658e858a17e8e81c77
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-15 12:17:54 -0500

    Copied the template and styling from the login view

--- src/View/SignUpView.tsx
+++ src/View/SignUpView.tsx
@@ -1,9 +1,23 @@
+import FormControl from "@mui/material/FormControl";
+import { TextField, Button } from "@mui/material";
+import BankHead from "../assets/BankingHeadingImg.png";
+import "./SignUpView.css";
+
 function SignUpView() {
-    return (
-      <>
-       
-        <h1>Sign Up</h1>
-      </>
-    );
-  }
-  export default SignUpView;
+  return (
+    <div className="sign-up-view">
+      <div className="img-container">
+        <img src={BankHead} alt="" />
+      </div>
+      <div className="form-container">
+        <h1>Sign up</h1>
+        <FormControl>
+          <TextField id="standard-basic" label="Username" variant="standard" />
+          <TextField id="standard-basic" label="Password" variant="standard" />
+          <Button variant="contained">Submit</Button>
+        </FormControl>
+      </div>
+    </div>
+  );
+}
+export default SignUpView;
