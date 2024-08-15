cdbd79648596fb93d9c9047924037f60a93f1ae6
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-15 12:21:15 -0500

    Added more form inputs

--- src/View/SignUpView.tsx
+++ src/View/SignUpView.tsx
@@ -12,7 +12,14 @@ function SignUpView() {
       <div className="form-container">
         <h1>Sign up</h1>
         <FormControl>
+          <TextField
+            id="standard-basic"
+            label="First Name"
+            variant="standard"
+          />
+          <TextField id="standard-basic" label="Last Name" variant="standard" />
           <TextField id="standard-basic" label="Username" variant="standard" />
+          <TextField id="standard-basic" label="Email" variant="standard" />
           <TextField id="standard-basic" label="Password" variant="standard" />
           <Button variant="contained">Submit</Button>
         </FormControl>
