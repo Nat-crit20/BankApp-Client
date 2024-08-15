5fed326c3767be4711f02277f6090db45fd1eead
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-15 11:57:38 -0500

    Added the from components and png

--- src/View/LoginView.tsx
+++ src/View/LoginView.tsx
@@ -1,8 +1,20 @@
+import FormControl from "@mui/material/FormControl";
+import { TextField, Button } from "@mui/material";
+import BankHead from "../assets/BankingHeadingImg.png";
+
 function LoginView() {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div>
+      <div>
+        <img src={BankHead} alt="" />
+      </div>
+      <h1>Log in</h1>
+      <FormControl>
+        <TextField id="standard-basic" label="Username" variant="standard" />
+        <TextField id="standard-basic" label="Password" variant="standard" />
+        <Button variant="contained">Submit</Button>
+      </FormControl>
+    </div>
   );
 }
 export default LoginView;
