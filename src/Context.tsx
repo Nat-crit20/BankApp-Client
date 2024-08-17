00f04b6367fb54b45d0460fc138a5a493d121d5a
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-17 12:44:34 -0500

    moved teh QuickStartReducer

--- src/Context.tsx
+++ src/Context.tsx
@@ -3,17 +3,20 @@ import { createContext, useReducer } from "react";
 export const linkIDContext = createContext(null);
 export const linkIDDispatchContext = createContext(null);
 
-export function QuickStartProvider({ children }) {
-  const [linkID, dispatch] = useReducer();
-}
+const initialState = {
+  linkTokenID: null,
+};
 
-function QuickStartReducer(state, action) {
-  switch (action.type) {
-    case "SET_STATE": {
-      return { ...state, ...action.type };
-    }
-    default: {
-      return { ...state };
+export function QuickStartProvider({ children }) {
+  function QuickStartReducer(state, action) {
+    switch (action.type) {
+      case "SET_STATE": {
+        return { ...state, ...action.type };
+      }
+      default: {
+        return { ...state };
+      }
     }
   }
+  const [state, dispatch] = useReducer(QuickStartReducer, initialState);
 }
