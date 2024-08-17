c5a2dff9705a147d30271d218868c512129b857b
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-17 13:06:43 -0500

    Created the QuickStateAction and add it to the QuickStartReducer

--- src/Context.tsx
+++ src/Context.tsx
@@ -2,19 +2,24 @@ import React, { createContext, useReducer, ReactNode, FC } from "react";
 interface QuickStartState {
   linkTokenID: string;
 }
+type QuickStartAction = {
+  type: string;
+  state: Partial<QuickStartState>;
+};
 const initialState: QuickStartState = {
   linkTokenID: "",
 };
+
 const Context = createContext(initialState);
 const { Provider } = Context;
 
 export const QuickStartProvider: FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  function QuickStartReducer(state, action) {
+  function QuickStartReducer(state: QuickStartState, action: QuickStartAction) {
     switch (action.type) {
       case "SET_STATE": {
-        return { ...state, ...action.type };
+        return { ...state, ...action.state };
       }
       default: {
         return { ...state };
