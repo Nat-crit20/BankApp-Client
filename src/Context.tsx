84bba269334b4931131055c1edfbcc8098a99d1e
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-17 12:42:25 -0500

    Created QuickStartReducer

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,3 +1,19 @@
 import { createContext, useReducer } from "react";
 
 export const linkIDContext = createContext(null);
+export const linkIDDispatchContext = createContext(null);
+
+export function QuickStartProvider({ children }) {
+  const [linkID, dispatch] = useReducer();
+}
+
+function QuickStartReducer(state, action) {
+  switch (action.type) {
+    case "SET_STATE": {
+      return { ...state, ...action.type };
+    }
+    default: {
+      return { ...state };
+    }
+  }
+}
