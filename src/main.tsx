95285c939c1f2619c206128620f1ec5646e2a854
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-17 13:14:07 -0500

    Added the QuickStartProvider to the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QuickStartProvider } from "./Context.tsx";
 
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QuickStartProvider>
+      <App />
+    </QuickStartProvider>
   </StrictMode>
 );
