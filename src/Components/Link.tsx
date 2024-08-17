7443b16b395f08700c78b0f4d3fd9bccb99fba8c
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-17 16:12:05 -0500

    Added the handleReadyAccess request to the useEffect

--- src/Components/Link.tsx
+++ src/Components/Link.tsx
@@ -7,29 +7,32 @@ interface LinkProps {
 }
 function Link({ handleReadyAccess }: LinkProps): JSX.Element {
   const { linkTokenID } = useContext(Context);
-  const onSuccess = useCallback((public_token: string) => {
-    const exchangePublicTokenForAccessToken = async () => {
-      const response = await fetch(
-        "http://localhost:3000/api/set_access_token",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-          },
-          body: `public_token=${public_token}`,
+  const onSuccess = useCallback(
+    (public_token: string) => {
+      const exchangePublicTokenForAccessToken = async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/set_access_token",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            },
+            body: `public_token=${public_token}`,
+          }
+        );
+        if (!response.ok) {
+          return;
         }
-      );
-      if (!response.ok) {
-        return;
-      }
-      const data = await response.json();
-      if (data) {
-        console.log("Success got the access token saved in the backend!");
-        handleReadyAccess();
-      }
-    };
-    exchangePublicTokenForAccessToken();
-  }, []);
+        const data = await response.json();
+        if (data) {
+          console.log("Success got the access token saved in the backend!");
+          handleReadyAccess();
+        }
+      };
+      exchangePublicTokenForAccessToken();
+    },
+    [handleReadyAccess]
+  );
 
   const { open, ready } = usePlaidLink({
     token: linkTokenID,
