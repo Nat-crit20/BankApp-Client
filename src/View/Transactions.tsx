bea3328b9cbc20bcca8e4d04d7994aa3301892f6
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-24 14:30:00 -0500

    Created the fetch method for getting the transations

--- src/View/Transactions.tsx
+++ src/View/Transactions.tsx
@@ -1,4 +1,24 @@
+import { useContext, useEffect } from "react";
+import Context from "../Context";
+
 const TransactionsView = () => {
+  const { accountAccess } = useContext(Context);
+
+  const getTransactions = async () => {
+    const response = await fetch("http://localhost:3000/api/transactions", {
+      method: "GET",
+    });
+    if (!response.ok) {
+      return;
+    }
+    const data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    if (accountAccess) {
+      getTransactions();
+    }
+  }, [accountAccess]);
   return (
     <>
       <h1>Transactions</h1>
