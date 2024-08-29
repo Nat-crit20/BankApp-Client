33cab066f655838b03bfc0ed7a0eb06513e5325b
Nat-crit20 <greuelnathan74@gmail.com>
2024-08-28 20:00:32 -0500

    Created the transaction typescript interface

--- src/View/Transactions.tsx
+++ src/View/Transactions.tsx
@@ -1,5 +1,24 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Context from "../Context";
+
+interface Transaction {
+  account_id: string;
+  amount: number;
+  authorized_date: Date;
+  category: string[];
+  date: Date;
+  iso_currency_code: string;
+  merchant_name: string;
+  payment_channel: string;
+  pending: boolean;
+  personal_finance_category: {
+    confidence_level: string;
+    detailed: string;
+    primary: string;
+  };
+  transaction_id: string;
+  website: string;
+}
 
 const TransactionsView = () => {
   const { accountAccess } = useContext(Context);
