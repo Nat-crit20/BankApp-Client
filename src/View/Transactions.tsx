76be67e9b9518f5743d340c56acbe42ba5a8cd2b
Nat-crit20 <greuelnathan74@gmail.com>
2024-09-02 11:11:47 -0500

    Created a utilies file

--- src/View/Transactions.tsx
+++ src/View/Transactions.tsx
@@ -1,25 +1,22 @@
 import { useContext, useEffect, useState } from "react";
 import Context from "../Context";
 import TransactionCard from "../Components/TransactionCard";
+import { getTransactions } from "../lib/utilities";
 import { Transaction } from "../lib/types";
 
 const TransactionsView = () => {
   const { accountAccess } = useContext(Context);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  const getTransactions = async () => {
-    const response = await fetch("http://localhost:3000/api/transactions", {
-      method: "GET",
-    });
-    if (!response.ok) {
-      return;
-    }
-    const data = await response.json();
-    setTransactions(data.latest_transactions);
-  };
   useEffect(() => {
     if (accountAccess) {
-      getTransactions();
+      const loadTransactions = async () => {
+        const data: Transaction[] = await getTransactions();
+        setTransactions(data);
+      };
+
+      // Call the function to load transactions
+      loadTransactions();
     }
   }, [accountAccess]);
   return (
---
+++ src/lib/utilities.ts
@@ -0,0 +1,10 @@
+export const getTransactions = async () => {
+  const response = await fetch("http://localhost:3000/api/transactions", {
+    method: "GET",
+  });
+  if (!response.ok) {
+    return [];
+  }
+  const data = await response.json();
+  return data.latest_transactions;
+};
