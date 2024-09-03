a408b59b7a3dd6c0186dac8ac8ade489f91978fa
Nat-crit20 <greuelnathan74@gmail.com>
2024-09-03 12:18:22 -0500

    Created the colums for the table

--- src/Components/TransactionTable.tsx
+++ src/Components/TransactionTable.tsx
@@ -3,9 +3,15 @@ import Paper from "@mui/material/Paper";
 
 import { Transaction } from "../lib/types";
 
-const columns: GridColDef[] = [];
+const columns: GridColDef[] = [
+  { field: "name", headerName: "Name", width: 170 },
+  { field: "transactionID", headerName: "TransactionId", width: 170 },
+  { field: "date", headerName: "Date", width: 170 },
+  { field: "status", headerName: "Status", width: 170 },
+  { field: "amount", headerName: "Amount", width: 170 },
+];
 
 const TransactionTable: React.FC<Transaction> = (transaction) => {
-  return <Card sx={{ minWidth: 275 }}></Card>;
+  return <Paper></Paper>;
 };
 export default TransactionTable;
