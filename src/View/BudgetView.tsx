fde4f83dcf718d615e3e58f08988f72f49c7212b
Nat-crit20 <greuelnathan74@gmail.com>
2024-09-15 18:53:17 -0500

    Created useEffect and added context

--- src/View/BudgetView.tsx
+++ src/View/BudgetView.tsx
@@ -1,6 +1,15 @@
+import { useContext, useEffect } from "react";
 import BasicPie from "../Components/PieChart";
+import Context from "../Context";
+import { getTransactions } from "../lib/utilities";
 
 const BudgetView = () => {
+  const { accountAccess } = useContext(Context);
+  useEffect(() => {
+    if (accountAccess) {
+      getTransactions();
+    }
+  });
   return (
     <>
       <h1>Budget Tab</h1>
