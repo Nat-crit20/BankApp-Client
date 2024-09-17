daefdec86f5b2b4dea38cac9c3f0ece4901c351f
Nat-crit20 <greuelnathan74@gmail.com>
2024-09-17 13:21:17 -0500

    Added Transaction useState and imported type interface

--- src/View/BudgetView.tsx
+++ src/View/BudgetView.tsx
@@ -1,15 +1,23 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import BasicPie from "../Components/PieChart";
 import Context from "../Context";
 import { getTransactions } from "../lib/utilities";
-
+import { Transaction } from "../lib/types";
 const BudgetView = () => {
   const { accountAccess } = useContext(Context);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
     if (accountAccess) {
-      getTransactions();
+      const loadTransactions = async () => {
+        const data: Transaction[] = await getTransactions();
+        setTransactions(data);
+      };
+
+      // Call the function to load transactions
+      loadTransactions();
     }
-  });
+  }, [accountAccess]);
   return (
     <>
       <h1>Budget Tab</h1>
