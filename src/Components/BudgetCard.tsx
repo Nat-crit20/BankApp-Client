b1dd874deb8ccb67d7e25b0558d39082ee77d655
Nat-crit20 <greuelnathan74@gmail.com>
2024-09-23 17:57:16 -0500

    Added the progress bar

--- src/Components/BudgetCard.tsx
+++ src/Components/BudgetCard.tsx
@@ -1,10 +1,25 @@
-import { Box, Card, CardContent } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  LinearProgress,
+  Typography,
+} from "@mui/material";
 
 const BudgetCard = () => {
   return (
     <Box>
       <Card variant="outlined">
-        <CardContent></CardContent>
+        <CardContent>
+          <Typography>Entertainment</Typography>
+          <LinearProgress color="success" value={45} />
+          <Box sx={{ minWidth: 35 }}>
+            <Typography
+              variant="body2"
+              sx={{ color: "text.secondary" }}
+            >{`45%`}</Typography>
+          </Box>
+        </CardContent>
       </Card>
     </Box>
   );
